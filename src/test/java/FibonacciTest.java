9febeab7a8c5ed14342f65cb5e5fdca07df003c0
Artur <arturjorgean@gmail.com>
2024-10-15 00:42:40 -0300

    test: add tests for usual numbers coverage

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,3 +1,5 @@
+import org.example.classes.Fibonacci;
+import org.example.exceptions.InvalidInputException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,4 +18,15 @@ public class FibonacciTest {
     public void testThatNegativeInputThrowsInvalidInputException() {
         assertThrows(InvalidInputException.class, () -> fibonacci.solveFibonacci(-1));
     }
+
+    @Test
+    public void testForSpecificNumbers() throws Exception {
+        assertEquals(0, fibonacci.solveFibonacci(0));
+        assertEquals(1, fibonacci.solveFibonacci(1));
+        assertEquals(2, fibonacci.solveFibonacci(1));
+        assertEquals(5, fibonacci.solveFibonacci(5));
+        assertEquals(32, fibonacci.solveFibonacci(2178309));
+        assertEquals(94, fibonacci.solveFibonacci(19740274219868000L));
+    }
+
 }
