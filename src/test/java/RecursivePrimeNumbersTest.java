765d9ae5979d6a50f5e05b99db9e939203219eec
Artur <arturjorgean@gmail.com>
2024-10-15 15:55:32 -0300

    test: add test coverage for recursion calculation

--- src/test/java/RecursivePrimeNumbersTest.java
+++ src/test/java/RecursivePrimeNumbersTest.java
@@ -3,6 +3,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.example.classes.RecursivePrimeNumbers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RecursivePrimeNumbersTest {
@@ -17,5 +21,15 @@ public class RecursivePrimeNumbersTest {
     @Test
     public void testThatNegativeInputThrowsInvalidInputException() {
         assertThrows(InvalidInputException.class, () -> RecursivePrimeNumbers.validatesInputNumber(0));
+    }
+
+    @Test
+    public void testForSpecificNumbers() throws Exception {
+        ArrayList<Integer> testArrayList = new ArrayList<>(Arrays.asList(2,3,5,7));
+        assertEquals(testArrayList, RecursivePrimeNumbers.solveRecursivePrimeNumbers(10));
+        ArrayList<Integer> testArrayList2 = new ArrayList<>(Arrays.asList(2,3,5));
+        assertEquals(testArrayList2, RecursivePrimeNumbers.solveRecursivePrimeNumbers(6));
+        ArrayList<Integer> testArrayList3 = new ArrayList<>(Arrays.asList(2,3,5,7,11,13,17,19,23));
+        assertEquals(testArrayList3, RecursivePrimeNumbers.solveRecursivePrimeNumbers(24));
     }
 }
