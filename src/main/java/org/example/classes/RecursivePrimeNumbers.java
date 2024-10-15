f0a859940fca7ce1905eae2fe16062ff0b8d4b73
Artur <arturjorgean@gmail.com>
2024-10-15 16:21:55 -0300

    feat: add recursive prime number check

--- src/main/java/org/example/classes/RecursivePrimeNumbers.java
+++ src/main/java/org/example/classes/RecursivePrimeNumbers.java
@@ -10,5 +10,34 @@ public class RecursivePrimeNumbers {
         if (targetFibonacciNumber < 2) throw new InvalidInputException("The input for this calculation can not be negative.");
     }
 
+    public static boolean checkIfNumberIsPrime(int numberToCheck) {
+        int checkingLimit = (int) Math.sqrt(numberToCheck);
 
+        if (numberToCheck < 2) {
+            return false;
+        }
+
+        for (int i = 2; i <= checkingLimit; i++) {
+            if (numberToCheck % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static ArrayList<Integer> solveRecursivePrimeNumbers(ArrayList<Integer> primeNumberList, int numberToCheck, int actualCheck) throws Exception {
+        validatesInputNumber(numberToCheck);
+
+        if (actualCheck > numberToCheck) {
+            return primeNumberList;
+        }
+
+        if (checkIfNumberIsPrime(actualCheck)) {
+            primeNumberList.add(actualCheck);
+        }
+
+        actualCheck++;
+        return solveRecursivePrimeNumbers(primeNumberList, numberToCheck, actualCheck);
+    }
 }
