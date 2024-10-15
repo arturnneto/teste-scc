a656e4ee7850bae9344ce5a551a607a4a16d44ae
Artur <arturjorgean@gmail.com>
2024-10-15 02:21:50 -0300

    test: update RecursiveFibonacci tests

--- src/test/java/RecursiveFibonacciTest.java
+++ src/test/java/RecursiveFibonacciTest.java
@@ -1,8 +1,10 @@
 import org.example.classes.Fibonacci;
+import org.example.classes.RecursiveFibonacci;
 import org.example.exceptions.InvalidInputException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RecursiveFibonacciTest {
@@ -16,6 +18,16 @@ public class RecursiveFibonacciTest {
 
     @Test
     public void testThatNegativeInputThrowsInvalidInputException() {
-        assertThrows(InvalidInputException.class, () -> RecursiveFibonacci.solveFibonacci(-1));
+        assertThrows(InvalidInputException.class, () -> RecursiveFibonacci.solveRecursiveFibonacci(-1));
+    }
+
+    @Test
+    public void testForSpecificNumbers() throws Exception {
+        assertEquals(0, RecursiveFibonacci.solveRecursiveFibonacci(0));
+        assertEquals(1, RecursiveFibonacci.solveRecursiveFibonacci(1));
+        assertEquals(1, RecursiveFibonacci.solveRecursiveFibonacci(2));
+        assertEquals(5, RecursiveFibonacci.solveRecursiveFibonacci(5));
+        assertEquals(610, RecursiveFibonacci.solveRecursiveFibonacci(15));
+        assertEquals(317811, RecursiveFibonacci.solveRecursiveFibonacci(28));
     }
 }
