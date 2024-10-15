0c4e22d4bedf6ae1090b204fbfb389289baf10ad
Artur <arturjorgean@gmail.com>
2024-10-15 13:16:00 -0300

    test: add tests for PrimeNumbers calculation

--- src/test/java/PrimeNumbersTest.java
+++ src/test/java/PrimeNumbersTest.java
@@ -1,8 +1,12 @@
-import org.example.classes.RecursiveFibonacci;
+import org.example.classes.PrimeNumbers;
 import org.example.exceptions.InvalidInputException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PrimeNumbersTest {
@@ -16,6 +20,16 @@ public class PrimeNumbersTest {
 
     @Test
     public void testThatNegativeInputThrowsInvalidInputException() {
-        assertThrows(InvalidInputException.class, () -> PrimeNumbers.solvePrimeNumbers(-1));
+        assertThrows(InvalidInputException.class, () -> PrimeNumbers.solvePrimeNumbers(0));
+    }
+
+    @Test
+    public void testForSpecificNumbers() throws Exception {
+        ArrayList<Integer> testArrayList = new ArrayList<>(Arrays.asList(2,3,5,7));
+        assertEquals(testArrayList, PrimeNumbers.solvePrimeNumbers(10));
+        ArrayList<Integer> testArrayList2 = new ArrayList<>(Arrays.asList(2,3,5));
+        assertEquals(testArrayList2, PrimeNumbers.solvePrimeNumbers(6));
+        ArrayList<Integer> testArrayList3 = new ArrayList<>(Arrays.asList(2,3,5,7,11,13,17,19,23));
+        assertEquals(testArrayList3, PrimeNumbers.solvePrimeNumbers(24));
     }
 }
