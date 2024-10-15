c3cb0b480a7aeb7dafa6908ff9122c5035a5e189
Artur <arturjorgean@gmail.com>
2024-10-15 15:54:33 -0300

    chore: add basic input validation

--- src/main/java/org/example/classes/RecursivePrimeNumbers.java
+++ src/main/java/org/example/classes/RecursivePrimeNumbers.java
@@ -1,2 +1,14 @@
-package org.example.classes;public class RecursivePrimeNumbers {
+package org.example.classes;
+
+import org.example.exceptions.InvalidInputException;
+
+import java.util.ArrayList;
+
+public class RecursivePrimeNumbers {
+
+    public static void validatesInputNumber(long targetFibonacciNumber) throws Exception {
+        if (targetFibonacciNumber < 2) throw new InvalidInputException("The input for this calculation can not be negative.");
+    }
+
+
 }
