4e9783ada5b18580793aff8e442d448579e77810
Artur <arturjorgean@gmail.com>
2024-10-15 15:25:45 -0300

    feat: add complete prime number calculation

--- src/main/java/org/example/classes/PrimeNumbers.java
+++ src/main/java/org/example/classes/PrimeNumbers.java
@@ -6,13 +6,38 @@ import java.util.ArrayList;
 
 public class PrimeNumbers {
 
-    public static ArrayList<Integer> solvePrimeNumbers(int number) throws Exception{
+    public static void validatesInputNumber(long targetFibonacciNumber) throws Exception {
+        if (targetFibonacciNumber < 2) throw new InvalidInputException("The input for this calculation can not be negative.");
+    }
+
+    public static boolean checkIfNumberIsPrime(int numberToCheck) {
+        int checkingLimit = (int) Math.sqrt(numberToCheck);
+
+        if (numberToCheck < 2) {
+            return false;
+        }
+
+        for (int i = 2; i <= checkingLimit; i++) {
+            if (numberToCheck % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static ArrayList<Integer> solvePrimeNumbers(int limitNumber) throws Exception {
+        validatesInputNumber(limitNumber);
         ArrayList<Integer> primeNumbers = new ArrayList<>();
 
-        if (number < 0) {
-            throw new InvalidInputException("The input for this calculation can not be negative.");
+        for (int i = 2; i <= limitNumber; i++) {
+            if (checkIfNumberIsPrime(i)) {
+                primeNumbers.add(i);
+            }
         }
 
         return primeNumbers;
     }
+
+
 }
