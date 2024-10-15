c354803922ea930c221649437b50764501b1335b
Artur <arturjorgean@gmail.com>
2024-10-15 01:06:18 -0300

    refactor: update Fibonacci code to complain with best practices and clean code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.classes.Fibonacci;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
+        System.out.println(Fibonacci.solveFibonacci(-5));
     }
 }
--- src/main/java/org/example/classes/Fibonacci.java
+++ src/main/java/org/example/classes/Fibonacci.java
@@ -3,21 +3,37 @@ package org.example.classes;
 import org.example.exceptions.InvalidInputException;
 
 public class Fibonacci {
-    public long solveFibonacci(long targetNumber) throws Exception {
-        if (targetNumber < 0) {
-            throw new InvalidInputException("The input for this calculation can not be negative.");
-        }
 
-        long firstNumber = 0;
-        long secondNumber = 1;
+    private static final long firstFibonacciNumber = 0L;
+    private static final long secondFibonacciNumber = 1L;
+
+    public static void validatesInputNumber(long targetFibonacciNumber) throws Exception {
+        if (targetFibonacciNumber < 0) throw new InvalidInputException("The input for this calculation can not be negative.");
+    }
+
+    public static long calculatesFibonacci(long targetFibonacciNumber) {
+        long firstNumberInSequence = firstFibonacciNumber;
+        long secondNumberInSequence = secondFibonacciNumber;
         long correspondingNumber = 0;
 
-        for (int i = 2; i <= targetNumber; i++) {
-            correspondingNumber = firstNumber + secondNumber;
-            firstNumber = secondNumber;
-            secondNumber = correspondingNumber;
+        if (targetFibonacciNumber == 0) {
+            return firstNumberInSequence;
+        }
+        if (targetFibonacciNumber == 1) {
+            return secondNumberInSequence;
         }
 
-        return correspondingNumber;
+        for (int i = 2; i <= targetFibonacciNumber; i++) {
+            correspondingNumber = firstNumberInSequence + secondNumberInSequence;
+            firstNumberInSequence = secondNumberInSequence;
+            secondNumberInSequence = correspondingNumber;
+        }
+
+        return  correspondingNumber;
+    }
+
+    public static long solveFibonacci(long targetNumber) throws Exception {
+        validatesInputNumber(targetNumber);
+        return calculatesFibonacci(targetNumber);
     }
 }
